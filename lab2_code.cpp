fa7f5ddacff576b3e149ffe47a0be34a53a3c718
kushalsangwan17 <53648987+kushalsangwan17@users.noreply.github.com>
2019-08-05 19:39:20 +0530

    Add files via upload

---
+++ lab2_code.cpp
@@ -0,0 +1,187 @@
+#include<iostream>
+using namespace std;
+
+struct node
+{
+	int x;
+	int y;
+	struct node * ptr;
+};
+
+typedef struct node node ;
+
+void add_first(int x, int y);
+void del_first();
+void del(int x, int y);
+void search(int d);
+void search(int x, int y);
+void length();
+
+node* head;
+
+int main(int argc, char const *argv[])
+{
+	head=new node;
+	head=NULL;
+	long long int num;
+	cin>>num;
+	int p,x,y;
+	float d;
+
+	while(num>0)
+	{
+		cin>>p;
+
+		switch(p)
+		{
+			case 1: cin>>x>>y; add_first(x,y); break;
+			case 2: del_first();	break;
+			case 3: cin>>x>>y; del(x,y); break;
+			case 4: cin>>d; search(d); break;
+			case 5: cin>>x>>y; search(x,y); break;
+			case 6: length(); break;
+		}
+		num--;
+	}
+	return 0;
+}
+
+void add_first(int x, int y)
+{
+	node* temp=new node;
+	temp->x=x;
+	temp->y=y;
+	temp->ptr=NULL;
+
+	if(head==NULL)
+		head=temp;
+	else
+	{
+		temp->ptr=head;
+		head=temp;
+	}
+}
+
+
+void del_first()
+{
+	if(head==NULL)
+	{
+		cout<<"empty linked list";
+	}
+
+	else
+	{
+		node* temp=new node;
+		temp=head->ptr;
+		free(head);
+		
+		head=temp;
+	}
+
+}
+void del(int x, int y)
+{
+	if(head==NULL)
+	{
+		cout<<"empty linked list";
+	}
+
+	else if(head->ptr==NULL)
+	{
+		if(head->x==x&&head->y==y)
+		{
+			del_first();
+		}
+		else cout<<"empty linked list";
+	}
+
+	else
+	{
+		node* temp=new node;
+		node* temp1=new node;
+		temp=head;
+		
+		if(temp->x==x&&temp->y==y)
+		{
+			del_first();
+			return;
+		}
+		
+
+		while(temp->ptr!=NULL)
+		{
+			if(temp->ptr->x==x&&temp->ptr->y==y) break;
+			temp=temp->ptr;
+
+		}
+
+		if(temp->ptr==NULL)
+			cout<<"node doesnt exist";
+		
+		else
+		{
+			temp1=temp->ptr->ptr;
+			free(temp->ptr);
+			temp->ptr=temp1;
+		}
+	}
+}
+void search(int d)
+
+{
+	if(head==NULL)
+	{
+		cout<<"empty linked list";
+	}
+
+	else
+	{
+		node* temp=new node;
+		temp=head;
+
+		while(temp!=NULL)
+		{
+			if(d*d>=temp->x*temp->x+temp->y*temp->y) cout<<"("<<temp->x<<","<<temp->y<<")"<<",";
+			temp=temp->ptr;
+
+		}
+	}
+}
+
+void search(int x, int y)
+
+{
+	if(head==NULL)
+	{
+		cout<<"empty linked list";
+	}
+
+	else
+	{
+		node* temp=new node;
+		temp=head;
+
+		while(temp!=NULL)
+		{
+			if(temp->x==x&&temp->y==y) cout<<"("<<temp->x<<","<<temp->y<<")"<<",";
+			temp=temp->ptr;
+
+		}
+	}
+}
+
+void length()
+{
+ 	int count=0;
+ 	node* temp = new node;
+ 	temp=head;
+ 	while(temp!=NULL)
+ 	{
+ 		count++;
+ 		temp=temp->ptr;
+ 	}
+ 	cout<<count;
+}
+
+
