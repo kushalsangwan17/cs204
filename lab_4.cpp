c31d4773c9a404138118662cf4c1a641a2023085
kushalsangwan17 <53648987+kushalsangwan17@users.noreply.github.com>
2019-08-19 18:30:27 +0530

    Update lab_4.cpp

--- lab_4.cpp
+++ lab_4.cpp
@@ -1,158 +1,177 @@
 
 
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define si size()
+#define bk back()
+#define popb pop_back()
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl "\n"
+#define lb lower_bound
+#define ub upper_bound
+#define emp empty()
+#define beg begin()
+#define en end()
+#define forl(i,a,n) for(int i=a;i<n;i++)
+#define rfor(i,n,a) for(int i=n;i>=a;i--)
+#define fore(i,a,n) for(int i=a;i<=n;i++)
+ll mod=1e9+7;
+//------------------------------------------------------------
+int prec(char c) 
+{ 
+    if(c == '^') 
+    return 3; 
+    else if(c == '*' || c == '/') 
+    return 2; 
+    else if(c == '+' || c == '-') 
+    return 1; 
+    else
+    return -1; 
+} 
+struct node{
+	int x;
+	char opr;
+	bool b;
+};
+struct et{
+	node val;
+	et * left;
+	et * right;
+};
 
-struct node
-{
-	string data;
-	struct node* left_child;
-	struct node* right_child;
-	struct node* parent;
-}
-
-class tree
-{
-	public:
-	node * root;
-}
-
-
-int evaulate(node* p)
-{
-	if(p->left_child==NULL)
-		return stoi(p->data);
-	else
-	{
-		x=evaulate(p->left_child);
-		y=evaulate(p->right_child);
-		switch(p->data)
-		{
-			case '+': return x+y;
-			case '-': return x-y;
-			case '*': return x*y;
-			case '/': return x/y;
-			case '^': return (int)pow(x,y);
+et * make_tree(vector<node>v){
+	stack<et *> s;
+	et *t, *t1, *t2;
+	forl(i,0,v.si){
+		if(v[i].b){
+			t=new et;
+			t->val=v[i];
+			t1=s.top();
+			s.pop();
+			t2=s.top();
+			s.pop();
+			t->right=t1;
+			t->left=t2;
+			s.push(t);
+		}
+		else{
+			t=new et;
+			t->val=v[i];
+			t->left=NULL;
+			t->right=NULL;
+			s.push(t);
 		}
 	}
+	return s.top();
 }
-
-tree buildexpressiontree(string s)
-{
-	for(int i=0;i<s.length();i++)
-	{
-		if(s[i]=='(') my_stack.push("(",1);
-		if(s[i]=='0'||s[i]=='1'||s[i]=='2'||s[i]=='3'||s[i]=='4'||s[i]=='5'||s[i]=='6'||s[i]=='7'||s[i]=='8'||s[i]=='9')
-		{
-			for(int j=1;i+j<s.length();j++)
-			{
-				if(!(s[i]=='0'||s[i]=='1'||s[i]=='2'||s[i]=='3'||s[i]=='4'||s[i]=='5'||s[i]=='6'||s[i]=='7'||s[i]=='8'||s[i]=='9'))
-				{
-					break;
+stack<node>st;
+vector<node> intopost(vector<node>v){
+	vector<node> ans;
+	forl(i,0,v.si){
+		if(v[i].b){
+			char c=v[i].opr;
+			if(c=='('){
+				st.push(v[i]);
+			}
+			else if(c==')'){
+				while(st.top().opr!='('){
+					ans.pb(st.top());
+					st.pop();
 				}
+				st.pop();
 			}
-
-			string s1=s.substr(i,j);
-			tree t=expression_tree1(s1);
-			stack.push(t);
-		}
-
-		if(s[i]=='*'||s[i]=='/'||s[i]=='^'||s[i]=='+'||s[i]=='-')
-		{
-			auto x=stack.pop();
-			if(stack.is_empty())
-			{	
-				stack.push(x);
-				tree e1=stack.pop();
-				string s3=stack.pop();
-				tree e2=stack.pop();
-				tree t=expression_tree(e1,s3,e2);
-				stack.push(t,0);
-			}
-
-			else
-			{
-				auto y=stack.pop();
-				int k=compare_priority(s[i],y);
-				if(k>0)
-					stack.push(s[i],1);
-				else
-				{	
-				stack.push(x);
-				stack.push(y);
-				tree e1=stack.pop();
-				string s3=stack.pop();
-				tree e2=stack.pop();
-				tree t=expression_tree(e1,s3,e2);
-				stack.push(t,0);
-				stack.push(s[i],1);
+			else{
+				while(st.top().opr!='('&&prec(c)<=prec(st.top().opr)){
+					if(c=='^'&&st.top().opr=='^') break;
+					ans.pb(st.top());
+					st.pop();
 				}
+				//if(st.top().opr=='(') st.pop();
+				st.push(v[i]);
 			}
-
-
-
 		}
-
-		if(s[i]==')')
-		{
-			while(1)
-			{
-				tree e1=stack.pop();
-				string s3=stack.pop();
-				tree e2=stack.pop();
-				tree t=expression_tree(e1,s3,e2);
-				if(stack.top()='(')break;
-			}
-				stack.push(t,0);
+		else{
+			ans.pb(v[i]);
 		}
 	}
-
-	tree t1=stack.pop()
-	return t1;
-}
-
-tree expressiontree1(string s)
-{
-	tree t;
-	node *p=new node;
 	
-	p->left_child=NULL;
-	p->right_child=NULL;
-	p->data=s;
-	tree.root=p;
-	return t;
-} 
-
-tree expressiontree1(tree t1,string s,tree t2)
-{
-	tree t;
-	node *p=new node;
-	p->left_child=t1.root();
-	p->right_child=t2.root();
-	p->data=s;
-	t.root=p;
-	return t;
-} 
-
-
-int main()
-{
-	int num;
-	cin>>num;
-	while(num>0)
-	{
-		int kk;
-		cin>>kk;
-		while(kk>0)
-		{	
-			string s;
-			cin>>s;
-			tree t=buildexpressiontree(s);
-			int i=evaulate(t.root());
-			cout<<i;
-			kk--;
-		}
-		num--;
-	}
 }
-
+int eval(et * root){
+	if(root==NULL) return 0;
+	if(root->left==NULL&&root->right==NULL){
+		return root->val.x;
+	}
+	int l=eval(root->left);
+	int r=eval(root->right);
+	if(root->val.opr=='+') return l+r;
+	else if(root->val.opr=='-') return l-r;
+	else if(root->val.opr=='*') return l*r;
+	else if(root->val.opr=='/') return l/r;
+	else return pow(l,r);
+}
+int main(){
+	fastio;
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		while(n--){
+			string s="(";
+			string tmps;
+			cin>>tmps;
+			s+=tmps;
+			s+=')';
+			int a=0,o=0;
+			string curr="";
+			vector<node> v;
+			forl(i,0,s.si){
+				if(s[i]>='0'&&s[i]<='9'){
+					curr+=s[i];
+				}
+				else{
+					if(!curr.si&&s[i]=='-'){
+						curr+=s[i];
+					}
+					else {
+						if(curr.si){
+						node n;
+						n.b=0;
+						n.x=stoi(curr);
+						v.pb(n);}
+						curr="";
+						node n;
+						n.opr=s[i];
+						n.b=1;
+						v.pb(n);
+					}
+				}
+			}
+			if(curr.si){
+				node n;
+				n.b=0;
+				n.x=stoi(curr);
+				v.pb(n);
+			}
+			//for(int i=0;i<v.si;i++){
+			//	if(v[i].b) cout<<v[i].opr<<endl;
+			//	else cout<<v[i].x<<endl;
+			//}
+			vector<node> post=intopost(v);
+			//forl(i,0,post.si){
+			//	if(post[i].b) cout<<post[i].opr;
+			//	else cout<<post[i].x;
+			//}
+			et * root=make_tree(post);
+			//cout<<root->right->left->val.x;
+			cout<<eval(root)<<endl;
+		}
+	}
+	return 0;
+}
